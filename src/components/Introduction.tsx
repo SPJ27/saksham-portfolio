863461ef1b36f28ea1f7b230444c0a688ea9289a
Saksham Jain <saksham.khatod27@gmail.com>
2025-02-05 20:56:00 +0530

    Update Introduction.tsx

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,16 +1,15 @@
 const Introduction = () => {
   return (
-    <div className=" max-auto px-4 sm:px-6 lg:px-8 py-14">
-      <div className=" max-w-3xl mx-auto items-center text-center justify-center">
-        Hi, I'm
+    <section className="max-auto px-4 sm:px-6 lg:px-8 py-14">
+      <div className="max-w-3xl mx-auto text-center">
+        <p className="text-lg text-gray-600">Hi, I'm</p>
+        <h1 className="font-bold text-zinc-950 text-5xl mt-2">Saksham</h1>
+        <p className="text-xl text-gray-600 mt-4">Full Stack Developer</p>
+        <p className="text-lg text-gray-600 mt-4">
+          I specialize in building web applications using modern technologies.
+        </p>
       </div>
-      <div className=" max-w-3xl text-5xl mx-auto items-center text-center justify-center">
-        Saksham 
-      </div>
-      <div className=" max-w-3xl mx-auto items-center text-center justify-center">
-        Full Stack Developer
-      </div>
-    </div>
+    </section>
   );
 };
 
